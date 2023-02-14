e91364971edafe2643d38e4c0506fe6a91d720df
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-02-14 23:21:28 +0300

    Input validation

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -3,7 +3,9 @@ import "./home.css";
 import Weather from "../../components/assets/cloud.png";
 import { Search } from "@material-ui/icons";
 import { Link } from "react-router-dom";
-import {} from "../../misc/misc0.js"
+import {} from "../../misc/misc0";
+import {} from "../../misc/misc1";
+import "../../misc/misc2.css";
 
 const data = [
   { id: 1, name: "Nairobi" },
@@ -42,6 +44,7 @@ const Home = () => {
             onChange={handleChange}
           />
         </form>
+        <p className="input-area-explanation"></p>
       </div>
       <div className="countyContainer">
         <span className="countyName">Nairobi</span>
